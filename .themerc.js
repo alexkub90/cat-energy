38ad57f35cb684fa7e8a974b37dd0900061b4daf
alexkub90 <alexkub90@gmail.com>
2023-02-21 16:03:34 +0500

    Отформатировал style.css

---
+++ .themerc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // For now, it only supports `less`, other languages will soon support.
+  type: 'less',
+  // you could config a local filePath or use module name like `require`
+  theme: './your_less_parameters_filePath.less'
+};
